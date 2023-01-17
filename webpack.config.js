c5a5a8119ee991ed18c80586ff092c6907c160a8
Jerad Gallinger <jerad@jeradgallinger.ca>
2023-01-17 02:03:30 -0500

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const postcssNormalize = require('postcss-normalize');
-const TerserJSPlugin = require('terser-webpack-plugin');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const postcssNormalize = require('postcss-normalize');
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./src/index.js",
   output: {
     filename: '[name].[contenthash].js',
-    path: __dirname + '/dist'
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
   },
   optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    minimize: true,
+    minimizer: [new TerserPlugin({}), new CssMinimizerPlugin()]
   },
   module: {
     rules: [
@@ -25,10 +36,12 @@ module.exports = {
           {
             loader: 'postcss-loader',
             options: {
-              ident: 'postcss',
-              plugins: () => [
-                postcssNormalize()
-              ]
+              // ident: 'postcss',
+              postcssOptions: {
+                plugins: () => [
+                  postcssNormalize()
+                ]
+              }
             }
           }
         ]
@@ -64,4 +77,13 @@ module.exports = {
       gaId: 'UA-168355220-1'
     })
   ]
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
 };
