3b9c782af4d8d6166ba323a66d42e605cd54065e
Jerad Gallinger <jerad@jeradgallinger.ca>
2023-02-05 23:17:19 -0500

    Use Handlebars for templating

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,38 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
+const { resolve } = require("path");
+const { readFileSync } = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const postcssNormalize = require('postcss-normalize');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const postcssNormalize = require("postcss-normalize");
+const Handlebars = require("handlebars");
+const { optimize: optimizeSvg } = require("svgo");
 
 const isProduction = process.env.NODE_ENV == "production";
 
-const stylesHandler = "style-loader";
+const PARTIALS = {
+  daggerStoriesLogoWhite: optimizeSvg(
+    readFileSync(resolve(__dirname, "./src/images/dagger-stories-logo-white.svg"), "utf-8"),
+    { path: "dagger-stories-logo-white.svg" }
+  ).data,
+};
+
+const TEMPLATE_VALUES = {
+  title: "Dagger Stories",
+  currentYear: (new Date()).getFullYear(),
+  description: "Platform-agnostic, non-fiction storytelling. Dagger Stories makes documentaries, reports from the field and manages productions. In addition to developing and running projects, we provide loan-outs for directing, producing, research and development services.",
+  typekitId: "tjo7hjt",
+  gaId: "UA-168355220-1",
+};
 
 const config = {
   entry: "./src/index.js",
   output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    path: resolve(__dirname, "dist"),
   },
   devServer: {
     open: true,
@@ -35,14 +51,38 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.hbs$/,
+        loader: "html-loader",
+        options: {
+          preprocessor: (content, loaderContext) => {
+            let result;
+
+            try {
+              Object.entries(PARTIALS).forEach(([name, partial]) => {
+                console.log({name: name})
+                console.log({partial: partial})
+                Handlebars.registerPartial(name, partial);
+              });
+
+              result = Handlebars.compile(content)(TEMPLATE_VALUES);
+            } catch (error) {
+              loaderContext.emitError(error);
+
+              return content;
+            }
+
+            return result;
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
+          "css-loader",
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              // ident: 'postcss',
               postcssOptions: {
                 plugins: () => [
                   postcssNormalize()
@@ -54,15 +94,8 @@ const config = {
       },
       {
         test: /\.ico$|\.gif$|\.jpg$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]' // <-- retain original file name
-            }
-          }
-        ]
-      }
+        type: "asset/resource"
+      },
     ]
   },
   plugins: [
@@ -70,17 +103,12 @@ const config = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css'
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].[hash].css"
     }),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: __dirname + '/index.html',
-      title: 'Dagger Stories',
-      currentYear: (new Date()).getFullYear(),
-      description: 'Platform-agnostic, non-fiction storytelling. Dagger Stories makes documentaries, reports from the field and manages productions. In addition to developing and running projects, we provide loan-outs for directing, producing, research and development services.',
-      typekitId: 'tjo7hjt',
-      gaId: 'UA-168355220-1'
+      template: "./src/index.hbs",
+      filename: __dirname + "/index.html",
     })
   ]
 };
